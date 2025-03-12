b509774e8b074ddb18d9f0e365e59b308fcc0856
SavitarN <nischalzoc999@gmail.com>
2025-03-12 22:28:55 +0545

    chages

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -13,8 +13,14 @@ const ShoeCard = ({ imgURL, changeBigShoe, bigShoeImg }) => {
       } cursor-pointer max-sm:flex-1`}
       onClick={handleClick}
     >
-      <div>
-        <img src={imgURL.thumbnail} />
+      <div className="flex justify-center items-center bg-card">
+        <img
+          src={imgURL.thumbnail}
+          alt="shoe collection"
+          width={127}
+          height={103}
+          className="object-contain "
+        />
       </div>
     </div>
   );
