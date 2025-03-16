2e10add5182cc3b9194fa0c486da0bc8f87a75fe
SavitarN <nischalzoc999@gmail.com>
2025-03-16 16:49:06 +0545

    shoe selection functionality

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 
-const ShoeCard = ({ imgURL, changeBigShoe, bigShoeImg }) => {
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
+  console.log("rendered shoe card");
   const handleClick = () => {
     if (bigShoeImg !== imgURL.bigShoe) {
-      changeBigShoe(imgURL.bigShoe);
+      changeBigShoeImage(imgURL.bigShoe);
     }
   };
   return (
     <div
       className={`border-2 rounded-xl ${
-        bigShoeImg === imgURL ? "border-coral-red" : "border-transparent"
-      } cursor-pointer max-sm:flex-1`}
+        bigShoeImg === imgURL.bigShoe
+          ? "border-coral-red"
+          : "border-transparent"
+      } cursor-pointer max-sm:flex-1 `}
       onClick={handleClick}
     >
-      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 border rounded-xl max-sm:p-4">
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 max-sm:p-4">
         <img
           src={imgURL.thumbnail}
           alt="shoe collection"
